10f2cbc4c3d2df53f39efe828ce50806f0cc67e2
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2024-10-08 21:56:36 +0530

    Create Customer Service Layer

--- src/main/java/lk/ijse/aadpossyastembackendassignment/service/CustomerService.java
+++ src/main/java/lk/ijse/aadpossyastembackendassignment/service/CustomerService.java
@@ -1,2 +1,4 @@
-package lk.ijse.aadpossyastembackendassignment.service;public interface CustomerService {
+package lk.ijse.aadpossyastembackendassignment.service;
+
+public interface CustomerService {
 }
--- src/main/java/lk/ijse/aadpossyastembackendassignment/service/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/aadpossyastembackendassignment/service/CustomerServiceImpl.java
@@ -1,2 +1,4 @@
-package lk.ijse.aadpossyastembackendassignment.service;public class CustomerServiceImpl {
+package lk.ijse.aadpossyastembackendassignment.service;
+
+public class CustomerServiceImpl implements CustomerService {
 }
