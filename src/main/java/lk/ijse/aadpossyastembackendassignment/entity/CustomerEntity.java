721658d0c90685ca2a61236b0a03f3fbe97188cb
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2024-10-09 23:12:41 +0530

    CustomerEntity implements SuperEntity

--- src/main/java/lk/ijse/aadpossyastembackendassignment/entity/CustomerEntity.java
+++ src/main/java/lk/ijse/aadpossyastembackendassignment/entity/CustomerEntity.java
@@ -1,2 +1,24 @@
-package lk.ijse.aadpossyastembackendassignment.entity;public class CustomerEntity {
+package lk.ijse.aadpossyastembackendassignment.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "customer")
+public class CustomerEntity implements SuperEntity {
+    @Id
+    private String id;
+    private String name;
+    private  String address;
+    private Double salary;
 }
