a97ddc446149801808245cd25ff92f3eed3540ea
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2024-10-09 23:13:47 +0530

    CustomerNotFoundExeption extends RuntimeException

--- src/main/java/lk/ijse/aadpossyastembackendassignment/exception/CustomerNotFoundExeption.java
+++ src/main/java/lk/ijse/aadpossyastembackendassignment/exception/CustomerNotFoundExeption.java
@@ -1,2 +1,7 @@
-package lk.ijse.aadpossyastembackendassignment.exception;public class CustomerNotFoundExeption {
+package lk.ijse.aadpossyastembackendassignment.exception;
+
+public class CustomerNotFoundExeption extends RuntimeException {
+    public CustomerNotFoundExeption() {}
+    public CustomerNotFoundExeption(String message) {}
+    public CustomerNotFoundExeption(String message, Throwable cause) {}
 }
