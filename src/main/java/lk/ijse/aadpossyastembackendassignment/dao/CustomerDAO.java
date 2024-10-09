6950abc2853df80a108853bdb0763de6fbf8bf4a
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2024-10-09 23:47:25 +0530

    Created CustomerDAO

--- src/main/java/lk/ijse/aadpossyastembackendassignment/dao/CustomerDAO.java
+++ src/main/java/lk/ijse/aadpossyastembackendassignment/dao/CustomerDAO.java
@@ -1,2 +1,11 @@
-package lk.ijse.aadpossyastembackendassignment.dao;public class CustomerDAO {
+package lk.ijse.aadpossyastembackendassignment.dao;
+
+import lk.ijse.aadpossyastembackendassignment.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerDAO extends JpaRepository<CustomerEntity, String> {
+    CustomerEntity getCustomerEntityByCustomerId(String customerId);
+
 }
