76758bf124e6d5b5435f57d7b0f84922e433aa99
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2024-10-10 19:29:51 +0530

    Create User ID

--- src/main/java/lk/ijse/aadpossyastembackendassignment/util/AppUtil.java
+++ src/main/java/lk/ijse/aadpossyastembackendassignment/util/AppUtil.java
@@ -1,7 +1,4 @@
 package lk.ijse.aadpossyastembackendassignment.util;
-
-
-import java.util.Base64;
 import java.util.UUID;
 
 public class AppUtil {
@@ -10,7 +7,4 @@ public class AppUtil {
         return "CUSTOMER-"+ UUID.randomUUID();
     }
 
-    public static String toBase64ProfilePic(String profilePic){
-        return Base64.getEncoder().encodeToString(profilePic.getBytes());
-    }
 }
