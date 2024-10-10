782c05a44615d91464f8b3ea7fb988c8638c14ad
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2024-10-10 19:50:09 +0530

    Optimize the process of customer

--- src/main/java/lk/ijse/aadpossyastembackendassignment/controller/CustomerController.java
+++ src/main/java/lk/ijse/aadpossyastembackendassignment/controller/CustomerController.java
@@ -1,6 +1,8 @@
 package lk.ijse.aadpossyastembackendassignment.controller;
 
+import lk.ijse.aadpossyastembackendassignment.customObj.CustomerResponse;
 import lk.ijse.aadpossyastembackendassignment.dto.impl.CustomerDTO;
+import lk.ijse.aadpossyastembackendassignment.exception.CustomerNotFoundExeption;
 import lk.ijse.aadpossyastembackendassignment.exception.DataPersistFailedException;
 import lk.ijse.aadpossyastembackendassignment.service.CustomerService;
 import lk.ijse.aadpossyastembackendassignment.util.AppUtil;
@@ -11,6 +13,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/customers")
 @RequiredArgsConstructor
@@ -18,30 +22,50 @@ public class CustomerController {
     @Autowired
     private final CustomerService customerService;
     //Save Customer
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Void> saveCustomer(
-            @RequestPart("Id") String Id,
-            @RequestPart("name") String Name,
-            @RequestPart("address") String Address,
-            @RequestPart("salary") Double Salary){
-        try {
-//            //byte [] imageByteCollection = profilePic.getBytes();
-//            String base64ProfilePic = AppUtil.toBase64ProfilePic(profilePic);
-//            // build the user object
-            CustomerDTO buildCustomerDTO = new CustomerDTO();
-            buildCustomerDTO.setId(Id);
-            buildCustomerDTO.setName(Name);
-            buildCustomerDTO.setAddress(Address);
-            buildCustomerDTO.setSalary(Salary);
-
-            //send to the service layer
-            customerService.saveCustomer(buildCustomerDTO);
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveCustomer(@RequestBody CustomerDTO customerDTO){
+           try{
+            customerService.saveCustomer(customerDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
         }catch (DataPersistFailedException e){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception e){
+               e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+    @PatchMapping ( value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateCustomer(@PathVariable("id") String  customerId , @RequestBody CustomerDTO customerDTO){
+        try {
+            customerDTO.setId(customerId);
+            customerService.updateCustomer(customerDTO);
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (CustomerNotFoundExeption e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e) {
+            return  new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable ("id") String customerId) {
+        try {
+            customerService.deleteCustomer(customerId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (CustomerNotFoundExeption e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public CustomerResponse getSelectedUser(@PathVariable ("id") String customerId){
+        return customerService.getSelectedCustomer(customerId);
+    }
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<CustomerDTO> getAllUsers(){
+        return customerService.getAllUsers();
     }
 
 
