e93cf1b55bb5869e04fa64b2b096abd398bb4443
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2024-10-11 23:45:54 +0530

    Tested the CRUD of Item and optimize the mapping

--- src/main/java/lk/ijse/aadpossyastembackendassignment/util/Mapping.java
+++ src/main/java/lk/ijse/aadpossyastembackendassignment/util/Mapping.java
@@ -2,7 +2,9 @@ package lk.ijse.aadpossyastembackendassignment.util;
 
 
 import lk.ijse.aadpossyastembackendassignment.dto.impl.CustomerDTO;
+import lk.ijse.aadpossyastembackendassignment.dto.impl.ItemDTO;
 import lk.ijse.aadpossyastembackendassignment.entity.CustomerEntity;
+import lk.ijse.aadpossyastembackendassignment.entity.ItemEntity;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,5 +27,15 @@ public class Mapping {
 
     public CustomerEntity convertToCustomerEntity(CustomerDTO customerDTO) {
         return modelMapper.map(customerDTO, CustomerEntity.class);
+    }
+    //matters of ItemEntity and DTO
+    public ItemDTO convertToDTO(ItemEntity note) {
+        return modelMapper.map(note, ItemDTO.class);
+    }
+    public ItemEntity convertToEntity(ItemDTO dto) {
+        return modelMapper.map(dto, ItemEntity.class);
+    }
+    public List<ItemDTO> convertToDTO(List<ItemEntity> notes) {
+        return modelMapper.map(notes, new TypeToken<List<ItemDTO>>() {}.getType());
     }
 }
