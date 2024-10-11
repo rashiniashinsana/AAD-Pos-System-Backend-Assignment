42e600dba4cb5dd2ac24c82721d416d4128d9dec
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2024-10-11 23:49:21 +0530

    Add Item Not Found Exception

---
+++ src/main/java/lk/ijse/aadpossyastembackendassignment/exception/ItemNotFound.java
@@ -0,0 +1,13 @@
+package lk.ijse.aadpossyastembackendassignment.exception;
+
+public class ItemNotFound extends RuntimeException{
+    public ItemNotFound(){
+
+    }
+    public ItemNotFound(String message){
+
+    }
+    public ItemNotFound(String message, Throwable cause){
+
+    }
+}
