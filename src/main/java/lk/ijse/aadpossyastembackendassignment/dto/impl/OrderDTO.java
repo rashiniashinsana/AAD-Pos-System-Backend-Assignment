bd8652b1ff338406a7f090c8274359c5d615d202
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2024-10-12 12:48:57 +0530

    Add Order DTO & Entity

--- src/main/java/lk/ijse/aadpossyastembackendassignment/dto/impl/OrderDTO.java
+++ src/main/java/lk/ijse/aadpossyastembackendassignment/dto/impl/OrderDTO.java
@@ -1,4 +1,21 @@
 package lk.ijse.aadpossyastembackendassignment.dto.impl;
 
-public class OrderDTO {
+import lk.ijse.aadpossyastembackendassignment.customObj.OrderResponse;
+import lk.ijse.aadpossyastembackendassignment.dto.SuperDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OrderDTO implements SuperDTO, OrderResponse {
+    private String id;
+    private String date;
+    private String customerId;
+    private double total;
+    private String discount;
+    private double subTotal;
+    private double cash;
+    private double balance;
 }
--- src/main/java/lk/ijse/aadpossyastembackendassignment/entity/OrderEntity.java
+++ src/main/java/lk/ijse/aadpossyastembackendassignment/entity/OrderEntity.java
@@ -1,4 +1,25 @@
 package lk.ijse.aadpossyastembackendassignment.entity;
 
-public class OrderEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "orders")
+public class OrderEntity implements SuperEntity{
+    @Id
+    private String id;
+    private String date;
+    private String customerId;
+    private double total;
+    private String discount;
+    private double subTotal;
+    private double cash;
+    private double balance;
 }
