8276fa86e301ebff8ddbcd5d679a73e63689197f
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2024-10-14 01:57:43 +0530

    Setup order crud

--- src/main/java/lk/ijse/aadpossyastembackendassignment/util/AppUtil.java
+++ src/main/java/lk/ijse/aadpossyastembackendassignment/util/AppUtil.java
@@ -6,5 +6,11 @@ public class AppUtil {
     public static String createCustomerId(){
         return "CUSTOMER-"+ UUID.randomUUID();
     }
+    public static String createItemId(){
+        return "ITEM-"+ UUID.randomUUID();
+    }
+    public static String createOrderId(){
+        return "ORDER-"+ UUID.randomUUID();
+    }
 
 }
