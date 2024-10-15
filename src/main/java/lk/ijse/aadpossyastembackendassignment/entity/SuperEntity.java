da6208f724cdc58fadaa968f6c45019095c127cc
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2024-10-15 11:54:39 +0530

    edited

--- src/main/java/lk/ijse/aadpossyastembackendassignment/entity/SuperEntity.java
+++ src/main/java/lk/ijse/aadpossyastembackendassignment/entity/SuperEntity.java
@@ -2,5 +2,5 @@ package lk.ijse.aadpossyastembackendassignment.entity;
 
 import java.io.Serializable;
 
-public interface SuperEntity extends Serializable {
+public interface  SuperEntity extends Serializable {
 }
