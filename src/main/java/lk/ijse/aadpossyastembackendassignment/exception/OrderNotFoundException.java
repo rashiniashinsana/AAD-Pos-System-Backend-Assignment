7d0a11d7c74d5e9c52b335b808899c26b0de35e7
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2024-10-15 12:00:41 +0530

    create exceptions

---
+++ src/main/java/lk/ijse/aadpossyastembackendassignment/exception/OrderNotFoundException.java
@@ -0,0 +1,9 @@
+package lk.ijse.aadpossyastembackendassignment.exception;
+
+public class OrderNotFoundException extends RuntimeException{
+    public OrderNotFoundException(){}
+
+    public OrderNotFoundException(String message){}
+
+    public OrderNotFoundException(String message, Throwable cause){}
+}
